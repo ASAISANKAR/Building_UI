76aa4257325cd76c251ab823e3d79a1228838124
akulasai <Sai.akula@cdk.com>
2026-06-13 20:51:44 +0530

    check

--- src/api/csrf_tokenservice.js
+++ src/api/csrf_tokenservice.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = `/data/csrf-token`;
+const API_BASE = `${process.env.REACT_APP_DEV_API}/csrf-token`;
 const getAuthHeaders = () => ({
     Authorization: `Bearer ${process.env.REACT_APP_Token}`,
 });
