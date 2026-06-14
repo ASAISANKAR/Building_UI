a3a6aa6324196e8404ee2c6fe593ea264a0f85ff
akulasai <Sai.akula@cdk.com>
2026-06-14 15:40:07 +0530

    Added ThemeProvider to use cdk stylings

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ThemeProvider} from "styled-components";
+import { theme } from '@cdkglobal/radial';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <QueryClientProvider client={new QueryClient()}>
+          <ThemeProvider theme={theme}>
         <App />
+          </ThemeProvider>
       </QueryClientProvider>
   </React.StrictMode>
 );
